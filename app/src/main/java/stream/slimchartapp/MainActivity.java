406ce3b999c9cf9c54a821fe31e58819936bb3cc
Ray Li <ray.li.verified@gmail.com>
2018-07-01 15:01:32 -0500

    Update AndroidX
    *Update to AndroidX.
    *Update Gradle to v3.2.0-beta02.
    *TargetSDK v28.

--- app/src/main/java/stream/slimchartapp/MainActivity.java
+++ app/src/main/java/stream/slimchartapp/MainActivity.java
@@ -2,7 +2,7 @@ package stream.slimchartapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
